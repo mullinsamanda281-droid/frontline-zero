e3297b31ac6d83ef3d56e657aeaa358c705dbb7c
mullinsamanda281-droid <mullinsamanda281-droid@users.noreply.github.com>
2026-08-01 12:13:05 -0400

    feat: desktop .deb packaging - Electron app, CI workflow, icon (#186-#195)

---
+++ electron/main.ts
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    icon: path.join(__dirname, '../public/icon.png'),
+  });
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  } else {
+    win.loadURL('http://localhost:5173');
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
